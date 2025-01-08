178e561831c21918a6e7a0d00fd9f0c28ce00542
Antho76 <62425937+Antho76@users.noreply.github.com>
2025-01-08 14:22:11 +0100

    jjn

--- Test_Watchdog.py
+++ Test_Watchdog.py
@@ -1,9 +1,8 @@
 import multiprocessing
 import time
 import os
+import threading
 
-last_disp_activity = 0
-last_serv_activity = 0
 serv_pid = 0
 disp_pid = 0 
 
@@ -12,14 +11,12 @@ def server(shared_value, fifo_path, sdfifo):
     serv_pid = os.getpid()
     print(f"Server process ID: {serv_pid}")
     
-    
-    #Ouverture du tube
-    with open(fifo_path,'r') as fifo:
+    # Ouverture du tube
+    with open(fifo_path, 'r') as fifo:
         while True:
-
-            global last_serv_activity
-            last_serv_activity = time.time()
-            #lecture du tube
+            last_serv_activity.value = int(time.time())  # Mettre à jour l'activité du serveur
+            
+            # Lecture du tube
             data = fifo.read()
             if not data:
                 print("Server breaking")
@@ -33,29 +30,41 @@ def server(shared_value, fifo_path, sdfifo):
                 case "HALF":
                     shared_value.value = shared_value.value // 2
 
-            with open(sdfifo,'w') as fifo2:
+            with open(sdfifo, 'w') as fifo2:
                 fifo2.write("done")
-                fifo.flush()
+                fifo2.flush()
             
             print(f"Server updated shared value to: {shared_value.value}")
 
 def watchdog():
     print("Watchdog launched")
+    print("Launching dispatcher....")
+    try:
+        dispatcher()
+    except:
+        print("")
+    finally:
+        print("Launched !")
+    
     global serv_pid
     global disp_pid
+
     while True:
-        time.sleep(5)
-        current_time = time.time()
-        if current_time-last_serv_activity > 10:
-            print("Serveur inactif depuis 10 sec")
-            os.kill(serv_pid,0)
         
-        if current_time-last_disp_activity > 10:
-            print("Dispatcher inactif depuis 10 sec")
-            os.kill(disp_pid,0)
+        time.sleep(2)
+        current_time = int(time.time())
+        #print(current_time, last_serv_activity.value)
+        if current_time - last_serv_activity.value > 10:
+            print("Serveur inactif depuis 10 sec, arrêt du serveur.")
+            os.kill(serv_pid, 9)  # Terminer le processus serveur
+            break  # Sortir de la boucle watchdog si le serveur est arrêté
+            
+        if current_time - last_disp_activity.value > 10:
+            print("Dispatcher inactif depuis 10 sec, arrêt du dispatcher.")
+            os.kill(disp_pid, 9)  # Terminer le processus dispatcher
+            break  # Sortir de la boucle watchdog si le dispatcher est arrêté
 
 def dispatcher():
-
     global disp_pid
     disp_pid = os.getpid()
 
@@ -64,9 +73,9 @@ def dispatcher():
         print(f"Vous avez entré le nombre : {nombre}")
 
     except ValueError:
-
         print("Ce n'est pas un nombre entier valide.")
-    
+        nombre = 0
+
     # Créer un tube 
     fifo_path = 'dsfifo'
     if not os.path.exists(fifo_path):
@@ -79,26 +88,21 @@ def dispatcher():
     # Créer un segment de mémoire partagée
     shared_value = multiprocessing.Value('i', nombre)  # 'i' pour un entier
 
-    
     # Créer le processus serveur
-    server_process = multiprocessing.Process(target=server, args=(shared_value,fifo_path,sdfifo))
-
-    
+    server_process = multiprocessing.Process(target=server, args=(shared_value, fifo_path, sdfifo))
     
     # Démarrer le serveur
     server_process.start()
     
 
-    global last_disp_activity
-    last_disp_activity= time.time()
+    last_disp_activity.value = int(time.time())
 
     with open(fifo_path, 'w') as fifo:
-
         fifo.write(f"SUM")
         fifo.flush()
 
-    with open(sdfifo,'r') as fifo2:
-        data=None
+    with open(sdfifo, 'r') as fifo2:
+        data = None
         while not data:
             data = fifo2.read()
             print("Waiting for server response")
@@ -108,10 +112,20 @@ def dispatcher():
     
     print(f"Final shared value: {shared_value.value}")
 
-    #Nettoyage tube
+    # Nettoyage tube
     os.remove(fifo_path)
+    os.remove(sdfifo)
 
 if __name__ == "__main__":
+
+    #Init variables
+    last_disp_activity = multiprocessing.Value('i',int(time.time()))
+    last_serv_activity = multiprocessing.Value('i',last_disp_activity.value)
+
+    # Démarrer le watchdog
+    watchdog_thread = threading.Thread(target=watchdog)
+    watchdog_thread.daemon = True
+    watchdog_thread.start()
+
     while True:
         dispatcher()
-        print("restarting disp")
